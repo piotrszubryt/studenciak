666252416b53ebe4cbb0105d0c3abe1216081edf
piotrekgolo <piogol52@interia.pl>
2015-06-02 18:48:39 +0200

    2 czerwca

--- src/Studenciak/StudentBundle/Controller/PageController.php
+++ src/Studenciak/StudentBundle/Controller/PageController.php
@@ -11,14 +11,39 @@ class PageController extends Controller
         //return $this->render('StudenciakBundle:Page:index.html.twig', array('name' => $name));
    // }
 
-    public function indexAction()
-    {
-    	return $this->render('StudenciakBundle:Page:dashboard.html.twig');
-    }
+	public function indexAction()
+	{
+		return $this->render('StudenciakBundle:Page:base.html.twig');
+	}
 
-    public function courseAction()
-    {
-    	return $this->render('StudenciakBundle:Page:dashboard.html.twig');
-    }
+	public function courseAction()
+	{
+		return $this->render('StudenciakBundle:Page:extend/course.html.twig');
+	}
+
+	public function repoAction()
+	{
+		return $this->render('StudenciakBundle:Page:extend/repo.html.twig');
+	}
+
+	public function groupsAction()
+	{
+		return $this->render('StudenciakBundle:Page:extend/groups.html.twig');
+	}
+
+	public function diaryAction()
+	{
+		return $this->render('StudenciakBundle:Page:extend/diary.html.twig');
+	}
+
+	public function profileAction()
+	{
+		return $this->render('StudenciakBundle:Page:extend/profile.html.twig');
+	}
+
+	public function logoutAction()
+	{
+		return $this->render('StudenciakBundle:Page:extend/logout.html.twig');
+	}
 
 }
